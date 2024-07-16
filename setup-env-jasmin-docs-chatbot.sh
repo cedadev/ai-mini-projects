3d9f933a9adfcef9fd661b7e09e42a1b38421350
Ag Stephens <ag.stephens@stfc.ac.uk>
2024-07-16 14:37:41 +0000

    Updated

--- setup-env-jasmin-docs-chatbot.sh
+++ setup-env-jasmin-docs-chatbot.sh
@@ -2,4 +2,4 @@ export PINECONE_API_KEY=$(cat ~/.pinecone_api_key)
 export OPENAI_API_KEY=$(cat ~/.openai_api_key)
 
 source ~/venvs/llm-rag-venv/bin/activate
-cd llm-rag-jasmin-docs/
+cd jasmin-docs-chatbot/
