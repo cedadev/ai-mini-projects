b6171106f5598caccadce16f30e5270bc1f736ad
Ag Stephens <ag.stephens@stfc.ac.uk>
2024-12-03 18:47:14 +0000

    Updated to include auth
    Added user/password approach.
    Added example dotenv file - to indicate which variables are required.

--- jasmin-docs-chatbot/app.py
+++ jasmin-docs-chatbot/app.py
@@ -1,7 +1,23 @@
+from typing import Optional
+import os
+
 import chainlit as cl
 from src.rag_lib import RAGController
 
 rc = RAGController()
+pw = os.environ.get("APP_PASSWORD", "")
+
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if password == pw and username.endswith("@stfc.ac.uk"):
+        return cl.User(
+            identifier=username, metadata={"role": "user", "provider": "credentials"}
+        )
+    else:
+        return None
 
 
 @cl.on_message
@@ -13,5 +29,3 @@ async def main(message: cl.Message):
     await cl.Message(
         content=rag_response, 
     ).send()
-
-
