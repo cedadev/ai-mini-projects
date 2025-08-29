d3bb262ddde525af53663c8a08814b8d2a7d64c7
Ag Stephens <ag.stephens@stfc.ac.uk>
2025-08-29 10:52:05 +0100

    Updated

--- jasmin-docs-chatbot/ceda-catalogue/moles_scraper.py
+++ jasmin-docs-chatbot/ceda-catalogue/moles_scraper.py
@@ -1,6 +1,18 @@
-import requests
 import time
-import json
+import os
+
+if os.environ['USER'] == 'root':
+    raise Exception('Do not run as root - probably run as vagrant')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cedamoles_site.settings'
+
+import django
+django.setup()
+
+from cedamoles_app.models import *
+
+
+
 
 
 INITIAL_URL = "https://api.catalogue.ceda.ac.uk/api/v2/observations/?format=json&?publicationStatus__in=published,citable"
@@ -10,12 +22,78 @@ NAME_MAP = {"timePeriod": "time", "geographicExtent": "bbox"}
 SPATIAL_MAP = {"east": "eastBoundLongitude", "north": "northBoundLatitude",
                "west": "westBoundLongitude", "south": "southBoundLatitude"}
 URL_TMPL = "https://catalogue.ceda.ac.uk/uuid/{uuid}/"
-WAIT = 0.5
+WAIT = 1
 DEBUG_LIMIT = None
+DEBUG = False
 
+
+
+def decode_projects(projects):
+    return [getattr(p, "title") for p in projects.all() if getattr(p, "title")]
+   
+
+def decode_phenomena(phenomena):
+    return [term.name for phen in phenomena.all() for term in phen.names.all() if not term.name.startswith("http")]
+
+def decode_bbox(geographicExtent):
+    try:
+        return {key: getattr(geographicExtent, exkey, "") for key, exkey in SPATIAL_MAP.items()}
+    except:
+        print(f"[WARN] No bbox found")
+        return None
+
+
+def decode_time(timePeriod):
+    return getattr(timePeriod, "startTime", ""), getattr(timePeriod, "endTime", "")
+
+
+def process_record(ob: Observation, fields: list=REQ_FIELDS):
+    result = {}
+    
+    for key in REQ_FIELDS:
+        # Check first for null values because lookups will fail on those
+        if not getattr(rec, key) or key not in ["projects"]: #LOOKUP_FIELDS:
+            value = getattr(rec, key)
+        else:
+            decoder = eval(f"decode_{NAME_MAP.get(key, key)}")
+            value = decoder(getattr(rec, key))
+
+        result[NAME_MAP.get(key, key)] = value
+        
+    # Add url to the dictionary
+    result["url"] = URL_TMPL.format(uuid=result["uuid"])
+    return result
+
+
+def main():
+
+    content = []
+    i = 0
+    chunk_size = 100
+    n = Observation.objects.count()
+    counter = 0
+    
+    while i < n:
+        print("Working on batch: [{i} - {i + chunk_size}]")
+        obs = Observation.objects.order_by("ob_id")[i:i+chunk_size]
+        for ob in obs:
+            content.append(process_record(ob))
+     
+        counter += 1
+        if DEBUG_LIMIT and counter > DEBUG_LIMIT:
+            break
+    
+        outfile = f"output_{counter:03d}.json"
+        with open(outfile, "w") as writer:
+            json.dump(content, writer)
+            
+        print(f"Wrote: {outfile}")
+        i += chunk_size
+
+API_version = """
 
 def url2json(url: str):
-    print(f"Downloading: {url}")
+    if DEBUG: print(f"Downloading: {url}")
     time.sleep(WAIT)
     return requests.get(url).json()
 
@@ -42,12 +120,14 @@ def decode_time(url: str):
 
 def parse(resp: dict, fields: list=REQ_FIELDS):
     records = resp["results"]
-    print(f"Working on {len(records)} records.")
+    count = len(records)
+    print(f"Working on {count} records.")
     
     results = [] 
 
-    for rec in records[:DEBUG_LIMIT]:
-        result = {} 
+    for i, rec in enumerate(records[:DEBUG_LIMIT]):
+        result = {}
+        
         for key in REQ_FIELDS:
             # Check first for null values because lookups will fail on those
             if not rec[key] or key not in LOOKUP_FIELDS:
@@ -61,6 +141,7 @@ def parse(resp: dict, fields: list=REQ_FIELDS):
         # Add url to the dictionary
         result["url"] = URL_TMPL.format(uuid=result["uuid"])
         results.append(result)
+        print(f"Processed {i+1} of {count} records.")
         
     return results
 
@@ -87,6 +168,7 @@ def main(url: str=INITIAL_URL):
         if DEBUG_LIMIT and c > DEBUG_LIMIT:
             break
 
+"""
 
 if __name__ == "__main__":
     main()
